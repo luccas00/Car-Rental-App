1e700cd038b8cc65c1edff70f727c85e19a32c17
Luccas Carneiro <luccas.carneiro@smartconsulting.com.br>
2026-07-16 21:49:24 -0300

    test(di): add Autofac integration tests

---
+++ LocadoraDeVeiculos/LocadoraDeVeiculos.DependencyInjection.Tests/AutofacConfigurationTests.cs
@@ -0,0 +1,85 @@
+﻿using Autofac;
+using LocadoraDeVeiculos.WindowsApp;
+using LocadoraDeVeiculos.WindowsApp.Features.Login;
+using LocadoraDeVeiculos.WindowsApp.Shared;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LocadoraDeVeiculos.DependencyInjection.Tests
+{
+    [TestClass]
+    public class AutofacConfigurationTests
+    {
+        [TestMethod]
+        public void DevePossuirTelaLoginRegistradaNoContainer()
+        {
+            bool estaRegistrada =
+                AutoFac.Container.IsRegistered<TelaLogin>();
+
+            Assert.IsTrue(estaRegistrada);
+        }
+
+        [TestMethod]
+        public void DevePossuirTelaPrincipalRegistradaNoContainer()
+        {
+            bool estaRegistrada =
+                AutoFac.Container.IsRegistered<TelaPrincipalForm>();
+
+            Assert.IsTrue(estaRegistrada);
+        }
+
+        [TestMethod]
+        public void DeveResolverTelaLoginPeloContainer()
+        {
+            using ILifetimeScope scope =
+                AutoFac.Container.BeginLifetimeScope();
+
+            using TelaLogin telaLogin =
+                scope.Resolve<TelaLogin>();
+
+            Assert.IsNotNull(telaLogin);
+        }
+
+        [TestMethod]
+        public void DeveResolverTelaPrincipalPeloContainer()
+        {
+            using ILifetimeScope scope =
+                AutoFac.Container.BeginLifetimeScope();
+
+            using TelaPrincipalForm telaPrincipal =
+                scope.Resolve<TelaPrincipalForm>();
+
+            Assert.IsNotNull(telaPrincipal);
+        }
+
+        [TestMethod]
+        public void DeveResolverFactoryDaTelaPrincipal()
+        {
+            using ILifetimeScope scope =
+                AutoFac.Container.BeginLifetimeScope();
+
+            Func<TelaPrincipalForm> factory =
+                scope.Resolve<Func<TelaPrincipalForm>>();
+
+            Assert.IsNotNull(factory);
+
+            using TelaPrincipalForm telaPrincipal = factory();
+
+            Assert.IsNotNull(telaPrincipal);
+        }
+
+        [TestMethod]
+        public void FactoryDeveCriarInstanciasDiferentesDaTelaPrincipal()
+        {
+            using ILifetimeScope scope =
+                AutoFac.Container.BeginLifetimeScope();
+
+            Func<TelaPrincipalForm> factory =
+                scope.Resolve<Func<TelaPrincipalForm>>();
+
+            using TelaPrincipalForm primeiraTela = factory();
+            using TelaPrincipalForm segundaTela = factory();
+
+            Assert.AreNotSame(primeiraTela, segundaTela);
+        }
+    }
+}
